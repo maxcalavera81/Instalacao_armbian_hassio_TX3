2cdc465525557d9418e41e7adb8e553c67b43ab5
Bruno Melo <43672635+maxcalavera81@users.noreply.github.com>
2023-07-30 20:35:27 +0100

    Update instalacao_homeassistant.sh

--- instalacao_homeassistant.sh
+++ instalacao_homeassistant.sh
@@ -5,10 +5,10 @@
 ##                                                                   ##
 #######################################################################
 #######################################################################
-set -o errexit  # Exit script when a command exits with non-zero status
-set -o errtrace # Exit on error inside any functions or sub-shells
-set -o nounset  # Exit script on use of an undefined variable
-set -o pipefail # Return exit status of the last command in the pipe that failed
+#set -o errexit  # Exit script when a command exits with non-zero status
+#set -o errtrace # Exit on error inside any functions or sub-shells
+#set -o nounset  # Exit script on use of an undefined variable
+#set -o pipefail # Return exit status of the last command in the pipe that failed
 
 # ==============================================================================
 # GLOBALS
